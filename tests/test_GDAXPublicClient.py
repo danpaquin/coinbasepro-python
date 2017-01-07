21ecf6ccea6a6cc2ac7a6ae3f3e377dbe0c492e4
mits9 <mits9@users.noreply.github.com>
2017-01-07 11:43:27 -0600

    Add test for GetProductOrderBook Level 1

--- tests/test_GDAXPublicClient.py
+++ tests/test_GDAXPublicClient.py
@@ -10,13 +10,24 @@ my_vcr = vcr.VCR(
     match_on=['uri', 'method'],
 )
 
+#NOTE THAT THESE TESTS ARE FOCUSED ON BTC-USD
+TEST_PRODUCT_ID = 'BTC-USD'
+
 class TestGDAXPublicClient(unittest.TestCase):
     def setUp(self):
-        self.GDAX = PublicClient()
+        #Only testing for BTC-USD
+        self.GDAX = PublicClient(product_id=TEST_PRODUCT_ID)
+
+    def test_PublicClientInitCorrectProductID(self):
+        self.assertEquals(self.GDAX.productId,TEST_PRODUCT_ID)
+
+    def test_PublicClientInitWrongProductID(self):
+        EurProductIdClient = PublicClient(product_id="BTC-EUR")
+        self.assertNotEquals(self.GDAX,EurProductIdClient)
 
     @my_vcr.use_cassette()
     def test_getProducts(self):
-        #Results from direct run on Jan 7, 2017
+        #Results from direct browser run on Jan 7, 2017
         correct = [
              {"id": "BTC-GBP", "base_currency": "BTC", "quote_currency": "GBP", "base_min_size": "0.01",
               "base_max_size": "10000", "quote_increment": "0.01", "display_name": "BTC/GBP"},
@@ -34,6 +45,17 @@ class TestGDAXPublicClient(unittest.TestCase):
               "base_max_size": "10000", "quote_increment": "0.01", "display_name": "BTC/USD"}
         ]
         self.assertEqual(self.GDAX.getProducts(), correct)
+
+    @my_vcr.use_cassette()
+    def test_getProductOrderBook_level_1(self):
+        #test for first level depth
+        test_depth = 1
+
+        #Results from direct browser run on Jan 7, 2017
+        correct_sequence = 1974627815
+        results = self.GDAX.getProductOrderBook(level=test_depth,\
+                                                       product=TEST_PRODUCT_ID)
+        self.assertEqual(results['sequence'], correct_sequence)
 
 if __name__ == '__main__':
     unittest.main()
