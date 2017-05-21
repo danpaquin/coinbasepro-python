88af94e990eda316097328ef586107664de1385f
Ted McCormack <uzbit@uzbit.net>
2017-05-21 00:58:44 -0700

    Added get_current_book, Fixed issue with segfault, Fixed issue with NoneType order

--- GDAX/OrderBook.py
+++ GDAX/OrderBook.py
@@ -13,177 +13,203 @@ from GDAX.WebsocketClient import WebsocketClient
 
 class OrderBook(WebsocketClient):
 
-    def __init__(self, product_id='BTC-USD'):
-        WebsocketClient.__init__(self, products=product_id)
-        self._asks = RBTree()
-        self._bids = RBTree()
-        self._client = PublicClient(product_id=product_id)
-        self._sequence = -1
+	def __init__(self, product_id='BTC-USD'):
+		WebsocketClient.__init__(self, products=product_id)
+		self._asks = RBTree()
+		self._bids = RBTree()
+		self._client = PublicClient(product_id=product_id)
+		self._sequence = -1
 
-    def onMessage(self, message):
-        sequence = message['sequence']
-        if self._sequence == -1:
-            self._asks = RBTree()
-            self._bids = RBTree()
-            res = self._client.getProductOrderBook(level=3)
-            for bid in res['bids']:
-                self.add({
-                    'id': bid[2],
-                    'side': 'buy',
-                    'price': float(bid[0]),
-                    'size': float(bid[1])
-                })
-            for ask in res['asks']:
-                self.add({
-                    'id': ask[2],
-                    'side': 'sell',
-                    'price': float(ask[0]),
-                    'size': float(ask[1])
-                })
-            self._sequence = res['sequence']
+	def onMessage(self, message):
+		sequence = message['sequence']
+		if self._sequence == -1:
+			self._asks = RBTree()
+			self._bids = RBTree()
+			res = self._client.getProductOrderBook(level=3)
+			for bid in res['bids']:
+				self.add({
+					'id': bid[2],
+					'side': 'buy',
+					'price': float(bid[0]),
+					'size': float(bid[1])
+				})
+			for ask in res['asks']:
+				self.add({
+					'id': ask[2],
+					'side': 'sell',
+					'price': float(ask[0]),
+					'size': float(ask[1])
+				})
+			self._sequence = res['sequence']
 
-        if sequence <= self._sequence:
-            return
-        elif sequence > self._sequence + 1:
-            self.close()
-            self.start()
-            return
+		if sequence <= self._sequence or sequence > self._sequence + 1:
+			print("Out of sequence!", sequence, self._sequence)
+			self._sequence = sequence
+			return
 
-        # print(message)
-        msg_type = message['type']
-        if msg_type == 'open':
-            self.add(message)
-        elif msg_type == 'done' and 'price' in message:
-            self.remove(message)
-        elif msg_type == 'match':
-            self.match(message)
-        elif msg_type == 'change':
-            self.change(message)
-        self._sequence = sequence
+		# print(message)
+		msg_type = message['type']
+		if msg_type == 'open':
+			self.add(message)
+		elif msg_type == 'done' and 'price' in message:
+			self.remove(message)
+		elif msg_type == 'match':
+			self.match(message)
+		elif msg_type == 'change':
+			self.change(message)
 
-        # bid = self.get_bid()
-        # bids = self.get_bids(bid)
-        # bid_depth = sum([b['size'] for b in bids])
-        # ask = self.get_ask()
-        # asks = self.get_asks(ask)
-        # ask_depth = sum([a['size'] for a in asks])
-        # print('bid: %f @ %f - ask: %f @ %f' % (bid_depth, bid, ask_depth, ask))
+		self._sequence = sequence
 
-    def add(self, order):
-        order = {
-            'id': order['order_id'] if 'order_id' in order else order['id'],
-            'side': order['side'],
-            'price': float(order['price']),
-            'size': float(order['size']) if 'size' in order else float(order['remaining_size'])
-        }
-        if order['side'] == 'buy':
-            bids = self.get_bids(order['price'])
-            if bids is None:
-                bids = [order]
-            else:
-                bids.append(order)
-            self.set_bids(order['price'], bids)
-        else:
-            asks = self.get_asks(order['price'])
-            if asks is None:
-                asks = [order]
-            else:
-                asks.append(order)
-            self.set_asks(order['price'], asks)
+		# bid = self.get_bid()
+		# bids = self.get_bids(bid)
+		# bid_depth = sum([b['size'] for b in bids])
+		# ask = self.get_ask()
+		# asks = self.get_asks(ask)
+		# ask_depth = sum([a['size'] for a in asks])
+		# print('bid: %f @ %f - ask: %f @ %f' % (bid_depth, bid, ask_depth, ask))
 
-    def remove(self, order):
-        price = float(order['price'])
-        if order['side'] == 'buy':
-            bids = self.get_bids(price)
-            if bids is not None:
-                bids = [o for o in bids if o['id'] != order['order_id']]
-                if len(bids) > 0:
-                    self.set_bids(price, bids)
-                else:
-                    self.remove_bids(price)
-        else:
-            asks = self.get_asks(price)
-            if asks is not None:
-                asks = [o for o in asks if o['id'] != order['order_id']]
-                if len(asks) > 0:
-                    self.set_asks(price, asks)
-                else:
-                    self.remove_asks(price)
+	def add(self, order):
+		order = {
+			'id': order['order_id'] if 'order_id' in order else order['id'],
+			'side': order['side'],
+			'price': float(order['price']),
+			'size': float(order['size']) if 'size' in order else float(order['remaining_size'])
+		}
+		if order['side'] == 'buy':
+			bids = self.get_bids(order['price'])
+			if bids is None:
+				bids = [order]
+			else:
+				bids.append(order)
+			self.set_bids(order['price'], bids)
+		else:
+			asks = self.get_asks(order['price'])
+			if asks is None:
+				asks = [order]
+			else:
+				asks.append(order)
+			self.set_asks(order['price'], asks)
 
-    def match(self, order):
-        size = float(order['size'])
-        price = float(order['price'])
+	def remove(self, order):
+		price = float(order['price'])
+		if order['side'] == 'buy':
+			bids = self.get_bids(price)
+			if bids is not None:
+				bids = [o for o in bids if o['id'] != order['order_id']]
+				if len(bids) > 0:
+					self.set_bids(price, bids)
+				else:
+					self.remove_bids(price)
+		else:
+			asks = self.get_asks(price)
+			if asks is not None:
+				asks = [o for o in asks if o['id'] != order['order_id']]
+				if len(asks) > 0:
+					self.set_asks(price, asks)
+				else:
+					self.remove_asks(price)
 
-        if order['side'] == 'buy':
-            bids = self.get_bids(price)
-            assert bids[0]['id'] == order['maker_order_id']
-            if bids[0]['size'] == size:
-                self.set_bids(price, bids[1:])
-            else:
-                bids[0]['size'] -= size
-                self.set_bids(price, bids)
-        else:
-            asks = self.get_asks(price)
-            assert asks[0]['id'] == order['maker_order_id']
-            if asks[0]['size'] == size:
-                self.set_asks(price, asks[1:])
-            else:
-                asks[0]['size'] -= size
-                self.set_asks(price, asks)
+	def match(self, order):
+		size = float(order['size'])
+		price = float(order['price'])
 
-    def change(self, order):
-        new_size = float(order['new_size'])
-        price = float(order['price'])
+		if order['side'] == 'buy':
+			bids = self.get_bids(price)
+			if not bids:
+				return
+			assert bids[0]['id'] == order['maker_order_id']
+			if bids[0]['size'] == size:
+				self.set_bids(price, bids[1:])
+			else:
+				bids[0]['size'] -= size
+				self.set_bids(price, bids)
+		else:
+			asks = self.get_asks(price)
+			if not asks:
+				return
+			assert asks[0]['id'] == order['maker_order_id']
+			if asks[0]['size'] == size:
+				self.set_asks(price, asks[1:])
+			else:
+				asks[0]['size'] -= size
+				self.set_asks(price, asks)
 
-        if order['side'] == 'buy':
-            bids = self.get_bids(price)
-            if bids is None or not any(o['id'] == order['order_id'] for o in bids):
-                return
-            index = map(itemgetter('id'), bids).index(order['order_id'])
-            bids[index]['size'] = new_size
-            self.set_bids(price, bids)
-        else:
-            asks = self.get_asks(price)
-            if asks is None or not any(o['id'] == order['order_id'] for o in asks):
-                return
-            index = map(itemgetter('id'), asks).index(order['order_id'])
-            asks[index]['size'] = new_size
-            self.set_asks(price, asks)
+	def change(self, order):
+		new_size = float(order['new_size'])
+		price = float(order['price'])
 
-        tree = self._asks if order['side'] == 'sell' else self._bids
-        node = tree.get(price)
+		if order['side'] == 'buy':
+			bids = self.get_bids(price)
+			if bids is None or not any(o['id'] == order['order_id'] for o in bids):
+				return
+			index = map(itemgetter('id'), bids).index(order['order_id'])
+			bids[index]['size'] = new_size
+			self.set_bids(price, bids)
+		else:
+			asks = self.get_asks(price)
+			if asks is None or not any(o['id'] == order['order_id'] for o in asks):
+				return
+			index = map(itemgetter('id'), asks).index(order['order_id'])
+			asks[index]['size'] = new_size
+			self.set_asks(price, asks)
 
-        if node is None or not any(o['id'] == order['order_id'] for o in node):
-            return
+		tree = self._asks if order['side'] == 'sell' else self._bids
+		node = tree.get(price)
 
-    def get_ask(self):
-        return self._asks.min_key()
+		if node is None or not any(o['id'] == order['order_id'] for o in node):
+			return
 
-    def get_asks(self, price):
-        return self._asks.get(price)
+	def get_current_book(self):
+		result = dict()
+		result['sequence'] = self._sequence
+		result['asks'] = list()
+		result['bids'] = list()
+		for ask in self._asks:
+			thisAsk = self._asks[ask]
+			for order in thisAsk:
+				result['asks'].append([
+					order['price'],
+					order['size'],
+					order['id'],
+				])
+		for bid in self._bids:
+			thisBid = self._bids[bid]
+			for order in thisBid:
+				result['bids'].append([
+					order['price'],
+					order['size'],
+					order['id'],
+				])
+		return result
 
-    def remove_asks(self, price):
-        self._asks.remove(price)
+	def get_ask(self):
+		return self._asks.min_key()
 
-    def set_asks(self, price, asks):
-        self._asks.insert(price, asks)
+	def get_asks(self, price):
+		return self._asks.get(price)
 
-    def get_bid(self):
-        return self._bids.max_key()
+	def remove_asks(self, price):
+		self._asks.remove(price)
 
-    def get_bids(self, price):
-        return self._bids.get(price)
+	def set_asks(self, price, asks):
+		self._asks.insert(price, asks)
 
-    def remove_bids(self, price):
-        self._bids.remove(price)
+	def get_bid(self):
+		return self._bids.max_key()
 
-    def set_bids(self, price, bids):
-        self._bids.insert(price, bids)
+	def get_bids(self, price):
+		return self._bids.get(price)
+
+	def remove_bids(self, price):
+		self._bids.remove(price)
+
+	def set_bids(self, price, bids):
+		self._bids.insert(price, bids)
 
 
 if __name__ == '__main__':
-    import time
-    order_book = OrderBook()
-    order_book.start()
-    time.sleep(10)
-    order_book.close()
+	import time
+	order_book = OrderBook()
+	order_book.start()
+	time.sleep(10)
+	order_book.close()
--- GDAX/WebsocketClient.py
+++ GDAX/WebsocketClient.py
@@ -11,95 +11,96 @@ from threading import Thread
 from websocket import create_connection
 
 class WebsocketClient(object):
-    def __init__(self, url=None, products=None, type=None):
-        if url is None:
-            url = "wss://ws-feed.gdax.com"
+	def __init__(self, url=None, products=None, type=None):
+		if url is None:
+			url = "wss://ws-feed.gdax.com"
 
-        self.url = url
-        self.products = products
-        self.type = type or "subscribe"
-        self.stop = None
-        self.ws = None
-        self.thread = None
+		self.url = url
+		self.products = products
+		self.type = "subscribe" #type or "subscribe"
+		self.stop = False
+		self.ws = None
+		self.thread = None
 
-    def start(self):
-        def _go():
-            self._connect()
-            self._listen()
+	def start(self):
+		def _go():
+			self._connect()
+			self._listen()
 
-        self.onOpen()
-        self.ws = create_connection(self.url)
-        self.thread = Thread(target=_go)
-        self.thread.start()
+		self.onOpen()
+		self.ws = create_connection(self.url)
+		self.thread = Thread(target=_go)
+		self.thread.start()
 
-    def _connect(self):
-        if self.products is None:
-            self.products = ["BTC-USD"]
-        elif not isinstance(self.products, list):
-            self.products = [self.products]
+	def _connect(self):
+		if self.products is None:
+			self.products = ["BTC-USD"]
+		elif not isinstance(self.products, list):
+			self.products = [self.products]
 
-        if self.url[-1] == "/":
-            self.url = self.url[:-1]
+		if self.url[-1] == "/":
+			self.url = self.url[:-1]
 
-        self.stop = False
-        sub_params = {'type': 'subscribe', 'product_ids': self.products}
-        self.ws.send(json.dumps(sub_params))
-        if self.type is "heartbeat":
-            sub_params = {"type": "heartbeat", "on": True}
-            self.ws.send(json.dumps(sub_params))
+		self.stop = False
+		sub_params = {'type': 'subscribe', 'product_ids': self.products}
+		self.ws.send(json.dumps(sub_params))
+		if self.type == "heartbeat":
+			sub_params = {"type": "heartbeat", "on": True}
+			self.ws.send(json.dumps(sub_params))
 
-    def _listen(self):
-        while not self.stop:
-            try:
-                msg = json.loads(self.ws.recv())
-            except Exception as e:
-                self.onError(e)
-                self.close()
-            else:
-                self.onMessage(msg)
+	def _listen(self):
+		while not self.stop:
+			try:
+				msg = json.loads(self.ws.recv())
+			except Exception as e:
+				self.onError(e)
+				self.close()
+			else:
+				self.onMessage(msg)
 
-    def close(self):
-        if self.stop is False:
-            if self.type is "heartbeat":
-                self.ws.send(json.dumps({"type": "heartbeat", "on": False}))
-            self.stop = True
-            self.onClose()
-            self.ws.close()
+	def close(self):
+		if not self.stop:
+			if self.type == "heartbeat":
+				self.ws.send(json.dumps({"type": "heartbeat", "on": False}))
+			self.onClose()
 
-    def onOpen(self):
-        print("-- Subscribed! --\n")
+	def onOpen(self):
+		print("-- Subscribed! --\n")
 
-    def onClose(self):
-        print("\n-- Socket Closed --")
+	def onClose(self):
+		self.stop = True
+		#self.thread = None
+		self.ws.close()
+		print("\n-- Socket Closed --")
 
-    def onMessage(self, msg):
-        print(msg)
+	def onMessage(self, msg):
+		print(msg)
 
-    def onError(self, e):
-        SystemError(e)
+	def onError(self, e):
+		SystemError(e)
 
 
 if __name__ == "__main__":
-    import GDAX, time
-    class myWebsocketClient(GDAX.WebsocketClient):
-        def onOpen(self):
-            self.url = "wss://ws-feed.gdax.com/"
-            self.products = ["BTC-USD", "ETH-USD"]
-            self.MessageCount = 0
-            print ("Lets count the messages!")
+	import GDAX, time
+	class myWebsocketClient(GDAX.WebsocketClient):
+		def onOpen(self):
+			self.url = "wss://ws-feed.gdax.com/"
+			self.products = ["BTC-USD", "ETH-USD"]
+			self.MessageCount = 0
+			print ("Lets count the messages!")
 
-        def onMessage(self, msg):
-            print ("Message type:", msg["type"], "\t@ %.3f" % float(msg["price"]))
-            self.MessageCount += 1
+		def onMessage(self, msg):
+			print ("Message type:", msg["type"], "\t@ %.3f" % float(msg["price"]))
+			self.MessageCount += 1
 
-        def onClose(self):
-            print ("-- Goodbye! --")
+		def onClose(self):
+			print ("-- Goodbye! --")
 
-    wsClient = myWebsocketClient()
-    wsClient.start()
-    print(wsClient.url, wsClient.products)
-    # Do some logic with the data
-    while (wsClient.MessageCount < 500):
-        print ("\nMessageCount =", "%i \n") % wsClient.MessageCount
-        time.sleep(1)
-    wsClient.close()
+	wsClient = myWebsocketClient()
+	wsClient.start()
+	print(wsClient.url, wsClient.products)
+	# Do some logic with the data
+	while (wsClient.MessageCount < 500):
+		print ("\nMessageCount =", "%i \n") % wsClient.MessageCount
+		time.sleep(1)
+	wsClient.close()
