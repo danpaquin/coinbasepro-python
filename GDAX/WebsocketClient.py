d8237743e30d193c353070fb227579f4c682c38c
dj <djstras963@gmail.com>
2017-05-26 10:47:31 -0400

    fixed type error with print formatter

--- GDAX/WebsocketClient.py
+++ GDAX/WebsocketClient.py
@@ -101,6 +101,6 @@ if __name__ == "__main__":
     print(wsClient.url, wsClient.products)
     # Do some logic with the data
     while (wsClient.MessageCount < 500):
-        print ("\nMessageCount =", "%i \n") % wsClient.MessageCount
+        print("\nMessageCount =", "%i \n" % wsClient.MessageCount)
         time.sleep(1)
     wsClient.close()
