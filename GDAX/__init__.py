296d5d4f9a5b6091750916465ad76b583c2129f5
Ted McCormack <uzbit@uzbit.net>
2017-05-31 21:50:31 -0700

    Forgot to include Ticker in __init__

--- GDAX/__init__.py
+++ GDAX/__init__.py
@@ -2,3 +2,4 @@ from GDAX.AuthenticatedClient import AuthenticatedClient
 from GDAX.PublicClient import PublicClient
 from GDAX.WebsocketClient import WebsocketClient
 from GDAX.OrderBook import OrderBook
+from GDAX.Ticker import Ticker
