4529246c63b9f0e44ad639a923a1c03d54fbfe70
Jeremy Jordan <jtjordan@ncsu.edu>
2017-10-13 10:44:37 -0400

    store api credentials and load into env vars

--- gdax/__init__.py
+++ gdax/__init__.py
@@ -2,3 +2,11 @@ from gdax.authenticated_client import AuthenticatedClient
 from gdax.public_client import PublicClient
 from gdax.websocket_client import WebsocketClient
 from gdax.order_book import OrderBook
+
+import json
+import os
+
+# Import API Keys
+with open('credentials.json') as creds:
+    credentials = json.load(creds)
+    os.environ.update(credentials)
