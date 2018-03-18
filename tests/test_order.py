273672f70e3bcf0e56e71b40cb451ef5c92d9214
Luke <lukeryan0@hotmail.co.uk>
2018-03-18 11:49:02 +0000

    A simple test to check connectivity to GDAX and how to submit a limit order

--- tests/test_order.py
+++ tests/test_order.py
@@ -2,10 +2,24 @@
 
 import gdax
 from gdax_config import settings
+import pytest
 
-def client():
+@pytest.fixture(scope='module')
+def gdax_client():
     return gdax.AuthenticatedClient(settings.api_key, settings.api_secret, settings.passphrase)
 
+@pytest.mark.usefixtures('client')
+class TestPublicClient(object):
+    @pytest.mark.parametrize('product', ['BCH-BTC'])
+    def test_get_products(product):
+        historical_rates = client.get_product_historic_rates(product)
+        assert type(historical_rates) is dict
+
+    @pytest.mark.parametrize('product', ['BCH-BTC'])
+    def test_limit_order(product):
+        client = gdax_client()
+        response = client.buy(product_id=product, quantity=1, time_in_force='GTC', size=1, price=0.00001)
+        assert response['message'] == 'Insufficient funds'
+
 if __name__=='__main__':
-    client = client()
-    print(client.get_product_historic_rates("BCH-BTC"))
+    TestPublicClient.test_limit_order('BCH-BTC')
