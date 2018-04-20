df1360dc8092f28b11859a4071e528b993486161
Mateusz Wykurz <wykurz@gmail.com>
2018-04-19 23:53:28 -0400

    Mode 'dateutil' to test requirements

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ install_requires = [
     'requests>=2.13,<3.0',
     'six>=1.10,<2.0',
     'websocket-client>=0.40',
-    'pymongo>=3.5,<4.0',
-    'dateutil>=2.6,<3.0'
+    'pymongo>=3.5,<4.0'
 ]
 
 tests_require = [
     'pytest>=3.3,<4.0',
     'pytest-cov>=2.5,<3.0',
+    'dateutil>=2.6,<3.0'
     ]
 
 setup(
