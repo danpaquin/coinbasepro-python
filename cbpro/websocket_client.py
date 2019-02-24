0eff72f958c5c61a9c15d6be7694c83102dc9a4f
nazariyv <nazariy@inbox.ru>
2019-02-24 20:17:40 +0000

    [+] thread namings

--- cbpro/websocket_client.py
+++ cbpro/websocket_client.py
@@ -17,9 +17,21 @@ from pymongo import MongoClient
 from cbpro.cbpro_auth import get_auth_headers
 
 
-class WebsocketClient(object):
-    def __init__(self, url="wss://ws-feed.pro.coinbase.com", products=None, message_type="subscribe", mongo_collection=None,
-                 should_print=True, auth=False, api_key="", api_secret="", api_passphrase="", channels=None):
+class WebsocketClient:
+    def __init__(self,
+                 url="wss://ws-feed.pro.coinbase.com",
+                 products=None,
+                 message_type="subscribe",
+                 mongo_collection=None,
+                 should_print=True,
+                 auth=False,
+                 api_key="",
+                 api_secret="",
+                 api_passphrase="",
+                 channels=None,
+                 thread_name='WebsocketClient',
+                 keepalive_thread_name='WebsocketClientKeepAlive'):
+
         self.url = url
         self.products = products
         self.channels = channels
@@ -43,8 +55,8 @@ class WebsocketClient(object):
 
         self.stop = False
         self.on_open()
-        self.thread = Thread(target=_go)
-        self.keepalive = Thread(target=self._keepalive)
+        self.thread = Thread(target=_go, name=thread_name)
+        self.keepalive = Thread(target=self._keepalive, name=keeaplive_thread_name)
         self.thread.start()
 
     def _connect(self):
