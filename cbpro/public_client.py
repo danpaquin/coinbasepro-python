a9827a286b8d73366fcc5c12b261a2b214f13e57
Angel Aponte <angelinthemachine@gmail.com>
2021-01-12 22:23:09 -0500

    Added check for empty result
    Is the Coinbase API returning empty results?

--- cbpro/public_client.py
+++ cbpro/public_client.py
@@ -301,29 +301,29 @@ class PublicClient(object):
         url = self.url + endpoint
         
         while True:
-            try:
-                r = self.session.get(url, params=params, auth=self.auth, timeout=30)
-                results = r.json()
-                for result in results:
-                    print(result)
+        #try:
+            r = self.session.get(url, params=params, auth=self.auth, timeout=30)
+            results = r.json()
+            for result in results:
+                if result != []:
                     yield result
-                # If there are no more pages, we're done. Otherwise update `after`
-                # param to get next page.
-                # If this request included `before` don't get any more pages - the
-                # cbpro API doesn't support multiple pages in that case.
-                if not r.headers.get('cb-after') or \
-                        params.get('before') is not None:
-                    #If a sleep_interval was sent, use it
-                    if "sleep_interval" in kwargs.keys():
-                        time.sleep(sleep_interval)
-                    break
-                else:
-                    params['after'] = r.headers['cb-after']
-                    if "sleep_interval" in kwargs.keys():
-                        time.sleep(sleep_interval)
-            except (ValueError, AttributeError):
+            # If there are no more pages, we're done. Otherwise update `after`
+            # param to get next page.
+            # If this request included `before` don't get any more pages - the
+            # cbpro API doesn't support multiple pages in that case.
+            if not r.headers.get('cb-after') or \
+                    params.get('before') is not None:
+                #If a sleep_interval was sent, use it
                 if "sleep_interval" in kwargs.keys():
-                        print(result)
-                        time.sleep(sleep_interval)
-                print(sys.exc_info(), flush=True)
-                continue
+                    time.sleep(sleep_interval)
+                break
+            else:
+                params['after'] = r.headers['cb-after']
+                if "sleep_interval" in kwargs.keys():
+                    time.sleep(sleep_interval)
+        #except (ValueError, AttributeError):
+        #    if "sleep_interval" in kwargs.keys():
+        #            print(result)
+        #            time.sleep(sleep_interval)
+        #    print(sys.exc_info(), flush=True)
+        #    continue
